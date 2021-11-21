00d236df86521122f51699d10d0a6b75408dbc8e
JOHN CARLOS ARRIETA ARRIETA <johncarlosarrietaarrieta@gmail.com>
2021-11-21 11:35:36 -0500

    Publicado desde AndroidStudio en clase de uso de Fragmentos en el Curso de Desarrollo de App para Android escritas en Kotlin para MINTIC - MisionTIC en la UniDeAntioquia

---
+++ app/src/main/java/co/gov/mintic/misiontic/udea/ciclo4/MainActivity.kt
@@ -0,0 +1,11 @@
+package co.gov.mintic.misiontic.udea.ciclo4
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
